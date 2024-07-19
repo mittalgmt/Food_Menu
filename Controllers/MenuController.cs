68607767007b0bf4d0178a28d56872163b381f62
mittalgmt <gamitmittal0817@outlook.com>
2024-07-19 17:33:24 +0530

    added search

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -13,9 +13,16 @@ namespace Menu.Controllers
         {
             _menuContext = menuContext;
         }
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-            return View(await _menuContext.Dishes.ToListAsync());
+            var dishes = from d in _menuContext.Dishes
+                       select d;
+            if(!string.IsNullOrEmpty(searchString))
+            {
+                dishes = dishes.Where(d => d.Name.Contains(searchString));
+                return View(await dishes.ToListAsync());
+            }
+            return View(await dishes.ToListAsync());
         }
 
         public async Task<IActionResult> Details (int? id)
